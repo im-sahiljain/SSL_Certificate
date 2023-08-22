ea3431eb3e33a79952b2362a5039a0acb70b6cbb
Sahil Jain <mr.sahiljain14@gmail.com>
2023-08-22 12:42:56 +0530

    Update check_ssl_expiry.py

--- check_ssl_expiry.py
+++ check_ssl_expiry.py
@@ -4,7 +4,7 @@ import datetime
 
 def check_ssl_expiry(domain):
     """Check the SSL expiry date for a domain."""
-
+    
     try:
         context = ssl.create_default_context()
         with socket.create_connection((domain, 443)) as sock:
@@ -14,12 +14,12 @@ def check_ssl_expiry(domain):
                 remaining_days = (expiry_date - datetime.datetime.utcnow()).days
 
                 if remaining_days <= 30:
-                    warning = f"* Domain: {domain}\n  * Warning: The SSL certificate for {domain} will expire in {remaining_days} days."
+                    warning = f"SSL Expiry Alert\n* Domain : {domain}\n* Warning : The SSL certificate for {domain} will expire in {remaining_days} days."
                 else:
-                    warning = f"* Domain: {domain}\n  * Warning: The SSL certificate for {domain} is not expiring within 30 days."
+                    warning = f"SSL Expiry Alert\n* Domain : {domain}\n* Warning : The SSL certificate for {domain} is not expiring within 30 days."
 
     except Exception as e:
-        warning = f"* Domain: {domain}\n  * Error checking SSL certificate: {str(e)}"
+        warning = f"SSL Expiry Alert\n* Domain : {domain}\n* Error checking SSL certificate for {domain}: {str(e)}"
 
     return warning
 
@@ -30,17 +30,9 @@ def main():
         for line in f:
             domains.append(line.strip())
 
-    warnings = []
-
     for domain in domains:
         warning = check_ssl_expiry(domain)
-        warnings.append(warning)
-
-    # Join the warnings into a single Slack-formatted message
-    slack_message = "\n".join(warnings)
-
-    # Print the Slack-formatted message
-    print(slack_message)
+        print(warning)
 
 if __name__ == "__main__":
     main()
