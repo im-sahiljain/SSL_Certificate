ae2196415b6e9e6c70174bb0a03d8ec280772e71
Sahil Jain <mr.sahiljain14@gmail.com>
2023-08-22 12:17:16 +0530

    Update check_ssl_expiry.py

--- check_ssl_expiry.py
+++ check_ssl_expiry.py
@@ -4,7 +4,7 @@ import datetime
 
 def check_ssl_expiry(domain):
     """Check the SSL expiry date for a domain."""
-    
+
     try:
         context = ssl.create_default_context()
         with socket.create_connection((domain, 443)) as sock:
@@ -14,23 +14,22 @@ def check_ssl_expiry(domain):
                 remaining_days = (expiry_date - datetime.datetime.utcnow()).days
 
                 if remaining_days <= 30:
-                    return f"{domain} will expire in {remaining_days} days. Please renew the certificate."
+                    return f"The SSL certificate for {domain} will expire in {remaining_days} days. Please renew the certificate."
                 else:
-                    return f"{domain} is not expiring within 30 days."
+                    return f"The SSL certificate for {domain} is not expiring within 30 days."
 
     except Exception as e:
         return f"Error checking SSL certificate for {domain}: {str(e)}"
 
 def main():
-    domains = []
+    warnings = []
 
     with open("domains.txt") as f:
         for line in f:
-            domains.append(line.strip())
-
-    for domain in domains:
-        warning = check_ssl_expiry(domain)
-        print(warning)
+            domain = line.strip()
+            warning = check_ssl_expiry(domain)
+            warnings.append(warning)
+            print(warning)
 
 if __name__ == "__main__":
     main()
