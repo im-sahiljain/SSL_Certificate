cbf04569bfb3387d7ac007c7cbc84edc2c99b0e3
Sahil Jain <mr.sahiljain14@gmail.com>
2023-08-22 12:23:11 +0530

    Update check_ssl_expiry.py

--- check_ssl_expiry.py
+++ check_ssl_expiry.py
@@ -1,11 +1,10 @@
 import ssl
 import socket
 import datetime
-import os
 
 def check_ssl_expiry(domain):
     """Check the SSL expiry date for a domain."""
-
+    
     try:
         context = ssl.create_default_context()
         with socket.create_connection((domain, 443)) as sock:
@@ -15,24 +14,23 @@ def check_ssl_expiry(domain):
                 remaining_days = (expiry_date - datetime.datetime.utcnow()).days
 
                 if remaining_days <= 30:
-                    warning = f"The SSL certificate for {domain} will expire in {remaining_days} days. Please renew the certificate."
+                    return f"* Domain : {domain}\n  * Warning : The SSL certificate for {domain} will expire in {remaining_days} days."
                 else:
-                    warning = f"The SSL certificate for {domain} is not expiring within 30 days."
-
-                # Export the domain and warning as environment variables
-                os.environ['DOMAIN'] = domain
-                os.environ['WARNING'] = warning
+                    return f"* Domain : {domain}\n  * Warning : The SSL certificate for {domain} is not expiring within 30 days."
 
     except Exception as e:
-        os.environ['DOMAIN'] = domain
-        os.environ['WARNING'] = f"Error checking SSL certificate for {domain}: {str(e)}"
+        return f"* Domain : {domain}\n  * Error checking SSL certificate: {str(e)}"
 
 def main():
+    domains = []
+
     with open("domains.txt") as f:
         for line in f:
-            domain = line.strip()
-            check_ssl_expiry(domain)
-            print(f"Domain: {os.environ['DOMAIN']}\nWarning: {os.environ['WARNING']}")
+            domains.append(line.strip())
+
+    for domain in domains:
+        warning = check_ssl_expiry(domain)
+        print(warning)
 
 if __name__ == "__main__":
     main()
