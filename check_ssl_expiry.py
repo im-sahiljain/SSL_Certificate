47739151131f580562d38dc6f063207aff208018
Sahil Jain <mr.sahiljain14@gmail.com>
2023-08-22 12:09:11 +0530

    Update check_ssl_expiry.py

--- check_ssl_expiry.py
+++ check_ssl_expiry.py
@@ -1,30 +1,36 @@
-import requests
-import certifi
+import ssl
+import socket
 import datetime
 
 def check_ssl_expiry(domain):
-  """Check the SSL expiry date for a domain."""
+    """Check the SSL expiry date for a domain."""
+    
+    try:
+        context = ssl.create_default_context()
+        with socket.create_connection((domain, 443)) as sock:
+            with context.wrap_socket(sock, server_hostname=domain) as ssock:
+                cert = ssock.getpeercert()
+                expiry_date = datetime.datetime.strptime(cert['notAfter'], "%b %d %H:%M:%S %Y %Z")
+                remaining_days = (expiry_date - datetime.datetime.utcnow()).days
 
-  cert = requests.get(f"https://{domain}:443/", verify=certifi.where())
-  cert_info = cert.json()
-  expiry_date = cert_info["notAfter"]
-  remaining_days = (expiry_date - datetime.datetime.utcnow()).days
+                if remaining_days <= 30:
+                    return f"{domain} will expire in {remaining_days} days. Please renew the certificate."
+                else:
+                    return f"{domain} is not expiring within 30 days."
 
-  if remaining_days <= 30:
-    return f"{domain} will expire in {remaining_days} days. Please renew the certificate."
-  else:
-    return f"{domain} is not expiring within 30 days."
+    except Exception as e:
+        return f"Error checking SSL certificate for {domain}: {str(e)}"
 
 def main():
-  domains = []
+    domains = []
 
-  with open("domains.txt") as f:
-    for line in f:
-      domains.append(line.strip())
+    with open("domains.txt") as f:
+        for line in f:
+            domains.append(line.strip())
 
-  for domain in domains:
-    warning = check_ssl_expiry(domain)
-    print(warning)
+    for domain in domains:
+        warning = check_ssl_expiry(domain)
+        print(warning)
 
 if __name__ == "__main__":
-  main()
+    main()
